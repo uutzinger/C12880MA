db10d9459b7bacec0bc3ebc35575045f617bfcae
Urs Utzinger <uutzinger@gmail.com>
2020-09-25 08:06:41 -0700

    Compatibility with latest ADC

--- README.md
+++ README.md
@@ -3,11 +3,11 @@ C12880 mini spectrometer code for Teensy 3.x
 
 This code reads Hamamatsu spectrometer using the spectrometer provided ADC trigger signal and DMA. 
 The ADC clock is adjustable but the Teensy is limitted to about 800kS/s.
-The ADC library for Teensy is needed with external triggering functions integrated (provided).
+The Teensy ADC library (with external cloclock option) is needed.
 
 The hardware can be purchased here https://groupgets.com/manufacturers/hamamatsu-photonics/products/c12880ma-micro-spectrometer
 
-Urs Utzinger, Summer 2019
+Urs Utzinger, Summer 2019, Fall 2020
 
 Connections to several pins on Teensy will need to be made:
 
@@ -15,16 +15,21 @@ C12880MA  => Teensy 3.2
 
 =========================================================================
 
-SPEC_CLK => 23 // From Teensy to Spectrometer
+SPEC_CLK => 23      // From Teensy to Spectrometer
 
-SPEC_CLK => 21 // CLK cycles are counted with Teensy interrupt, pin 21 and 23 both need to be connected to SPEC_CLK
+SPEC_CLK => 21      // CLK cycles are counted with Teensy interrupt, pin 21 and 23 both need to be connected to SPEC_CLK
 
-SPEC_TRG => 11 // From Spectrometer to Teensy, ADC signal trigger
+SPEC_TRG => 11      // From Spectrometer to Teensy, ADC signal trigger
 
-SPEC_ST  => 12 // Sensor start pulse from Teensy
+SPEC_ST  => 12      // Sensor start pulse from Teensy
 
-SPEC_VIDEO => 14 // Sensor signal, analog in, buffered, low impedance to Teensy
+SPEC_VIDEO => 14    // Sensor signal, analog in, buffered, low impedance to Teensy
 
-SPEC_EOS  => 13 // Sensor End of Scan, to Teensy
+SPEC_EOS  => 13     // Sensor End of Scan, to Teensy
 
 FLASH_TRIGGER => 18 // Light on/off from Teensy, will also work on other pins
+
+Connect either 5V or 3.3V to Groupgets board, not both.
+
+Do not place the Groupgets board onto your desk without first attaching insultation ontp the back of the PCB.
+If the board is no longer working, likely the voltage regulator was shorted and needs to be replaced.
--- Teensy_Code/C12880MA/C12880MA.ino
+++ Teensy_Code/C12880MA/C12880MA.ino
@@ -33,15 +33,15 @@
 // The SPECTROMETER VIDEO signal will need to be connected to an analog input pin on the Teensy.
 // The SPECTROMTER EOS (end of scan) signal will need to be connected to the SPEC_EOS pin on
 // the Teensy.
-// SPEC_CLK             23 
-// INT_Pin              21 // connected also to SPEC_CLK to count clocks
-// SPEC_TRG             11 // ADC signal trigger to microcontroller, needs to go to pin 11
-// SPEC_ST              12 // Sensor start pulse from microcontroller
-// SPEC_VIDEO           14 // Sensor signal, analog in, buffered, low impedance to microcontroller
-// SPEC_EOS             13 // Sensor End of Scan, to microcontroller
+// SPEC_CLK             23 // The teensy generated master clock (output)
+// INT_Pin              21 // connected also to SPEC_CLK to count clocks (input)
+// SPEC_TRG             11 // ADC signal trigger to microcontroller, needs to go to pin 11 (input)
+// SPEC_ST              12 // Sensor start pulse from microcontroller (output)
+// SPEC_VIDEO           14 // Sensor signal, analog in, buffered, low impedance to microcontroller (input)
+// SPEC_EOS             13 // Sensor End of Scan, to microcontroller (input)
 // The above connections are mandatory. 
-// FLASH_TRIGGER        18 // Light on/off from microcontroller
-// LASER_TRIGGER        17 // 405 Laser on/off from microcontroller
+// FLASH_TRIGGER        18 // Light on/off from microcontroller (output)
+// LASER_TRIGGER        17 // 405 Laser on/off from microcontroller (output)
 // The above connection is voluntary. 
 //
 // Some breakout boards such as the 
@@ -62,7 +62,7 @@
 // If we had a 5M samples/sec ADC conversion available we could measure up to 12,000 spectra
 // per second and for transfer to the host, USB 2.0 would be needed.
 //
-// Urs Utzinger, March 2019, Tucson Arizona, USA
+// Urs Utzinger, March 2019, Fall 2020, Tucson Arizona, USA
 //
 // *********************************************************************************************//
 // Copyright (c) 2019 - Urs Utzinger
@@ -89,43 +89,45 @@
 #include "ADC.h"
 #include <DMAChannel.h>
 
-#define C12880_NUM_CHANNELS 288 // Number of spectral channels
-#define SPEC_TRG             11 // ADC signal trigger to microcontroller, needs to go to pin 11
-#define SPEC_ST              12 // Sensor start pulse from microcontroller
-#define SPEC_CLK             23 // Sensor clock from microcontroller
-#define SPEC_VIDEO           14 // Sensor signal, buffered, low impedance to microcontroller
-#define SPEC_EOS             13 // Sensor End of Scan, to microcontroller
-#define FLASH_TRIGGER        18 // Light on/off from microcontroller
-#define LASER_TRIGGER        17 // Light on/off from microcontroller
-#define INT_Pin              21 // Connect to SPEC_CLK to count clock cycles
+#define C12880_NUM_CHANNELS 288 // Number of spectral channels (output)
+#define SPEC_TRG             11 // ADC signal trigger to microcontroller, needs to go to pin 11 (input)
+#define SPEC_ST              12 // Sensor start pulse from microcontroller (output)
+#define SPEC_CLK             23 // Sensor clock from microcontroller (output)
+#define SPEC_VIDEO           14 // Sensor signal, buffered, low impedance to microcontroller (input)
+#define SPEC_EOS             13 // Sensor End of Scan, to microcontroller (input)
+#define FLASH_TRIGGER        18 // Light on/off from microcontroller (output)
+#define LASER_TRIGGER        17 // Light on/off from microcontroller (input)
+#define INT_Pin              21 // Connect to SPEC_CLK to count clock cycles (input)
 // ----------------------------------------------------------
-// PWM pins can be 3,4,5,6,9,10,20,21,22,23,25,32
-// ADC pins can be 14,15,16,17,18,19,20,21,22,23,26,27,28, 29,30,31,A10,A11,A12,A13
+// PWM pins can be 3,4,5,6,9,10,20,21,22,23,25,32 (for SPEC_CLK)
+// ADC pins can be 14,15,16,17,18,19,20,21,22,23,26,27,28, 29,30,31,A10,A11,A12,A13 (for SPEC_VIDEO)
 // Pin 11 is used for ADC trigger and can not be changed
-// SERIAL_TX and RX can be: 1,0 3,4
+// SERIAL_TX and RX can be: 1,0 3,4 (to communicate with other microcontroller)
 // EOS, FLASH, INT_PIN can be: 0..23, 24..33
 // https://www.pjrc.com/teensy/pinout.html
 // ----------------------------------------------------------
-// RX/TX will be used to connect Teensy to host controller via RX/TX (if host does not have USB)
+// RX/TX can be used to connect Teensy to host controller via RX/TX (if host does not have USB)
 // This will require using the serial libary
 // https://www.pjrc.com/teensy/td_uart.html
 #define mySerial Serial         // Communicatge with USB
-// #define mySerial Serial1        // Hardware Serial, communicate with RX,TX bins (0,1) to other microcontroller
-#define SERIAL_TX             1 // Serial transmission
-#define SERIAL_RX             0 // Serial receiving
-#define BAUDRATE         115200 // 2000000 works likely also
+// #define mySerial Serial1     // Hardware Serial, communicate with RX,TX bins (0,1) to other microcontroller
+#define SERIAL_TX             1 // Serial transmission pin
+#define SERIAL_RX             0 // Serial receiving pin
+#define BAUDRATE         115200 // 2000000 works likely also, for USB communication, baudrate is irrelevant
 
 // ----------------------------------------------------------
-// States:
+// Machine States:
 // ----------------------------------------------------------
-// Waiting:     Do nothing related to sensor readout
-// PreStart:    Prepare start
-// Start_ST:    Run 1 clock cycle then set ST high
-// Stop_ST:     Run 3 clocks + integrationtime - 48 clocks then set ST low
-// Pre_Readout: Run 48 + 40 clock cycles then start ADC
-// Readout:     Run C12880_NUM_CHANNELS clocks for readout
+// Waiting:      Do nothing related to sensor readout
+// PreStart:     Prepare start
+// Start_ST:     Run 1 clock cycle then set ST high
+// Stop_ST:      Run 3 clocks + integrationtime - 48 clocks then set ST low
+// Pre_Readout:  Run 48 + 40 clock cycles then start ADC
+// Readout:      Run C12880_NUM_CHANNELS clocks for readout
+// UserInput:    Reading and processing serial command inputs
+// TransferData: Sending spectrum over USB or serial connection
 // ----------------------------------------------------------
-// [In this implementation ADC is started when ST goes low and stopped when EOS goes high]
+// [In this implementation ADC is started when ST goes low and stopps when EOS goes high]
 
 // Volatile variables used in interrupt service routines
 enum states {preStart, startST, stopST, preReadout, readout, waiting, userInput, trasferData};
@@ -139,24 +141,24 @@ volatile bool     flashEnabled = false;
 volatile bool     LASEREnabled = false;
 
 // Regular global variables
-float  integrationTime = 0.01;
+float  integrationTime = 0.01;                           // Sensor exposure time in [s]
 uint16_t CLK_Pin       = SPEC_CLK;                       // Pin to connect spectrometer clock
 uint32_t CPU_Frequency = F_CPU/1E6;                      // MHz
-uint32_t CLK_Frequency = 200000;                         // Clock Frequency
+uint32_t CLK_Frequency = 200000;                         // ADC Clock Frequency
 uint32_t PWM_MaxFreq;                                    //
-uint16_t PWM_Resolution=4;                               // 4: 0..15
+uint16_t PWM_Resolution= 4;                              // 4: 0..15
 uint16_t PWM_MaxValue;                                   // if resolution is 4 then max value is 15 pow(2,PWM_Resolution)
 float    PWM_Duty      = 50.0;                           // a nice symmetric clock
 bool     highSpeed     = true;                           // Very high speed for sampling and conversion
-bool     verboseMode   = true;                           // Transmitt date in verbose mode
-bool     continous     = false;                          // Dont start with continous transmission
-bool     aPlotter      = false;                          // Format data for Arduino serial plotter
+bool     verboseMode   = true;                           // Transmitt date in verbose mode or binary
+bool     continous     = false;                          // Dont run measurements continously
+bool     aPlotter      = false;                          // Format data output for Arduino serial plotter
 
 
 /******************************************************************************/
 ADC *adc = new ADC();
 DMAChannel dma0;
-DMAMEM static uint16_t spectrum[C12880_NUM_CHANNELS+88+1];         // buffer, 
+DMAMEM static uint16_t spectrum[C12880_NUM_CHANNELS+88+1];       // DMA buffer, 
 // ADC starts recording when ST signal goes low and continues until EOS 
 // goes high. Therfore ADC buffer should be 88 cyles longer in beginning and 
 // one cycle longer at end compared to the number of spectral channels available.
@@ -166,7 +168,7 @@ DMAMEM static uint16_t spectrum[C12880_NUM_CHANNELS+88+1];         // buffer,
 // Clock Counter and State Machine
 /******************************************************************************/
 // This interrupt service routine counts the clk pulses sent to spectrometer 
-// on the INT_pin and creates the ST signal 
+// on the INT_pin and creates the ST signal
 // [The CLK is produced with analogWrite for the C12880 SPEC_CLK pin]
 // ----------------------------------------------------------
 // ST Cycle minimum length    381/f_clk
@@ -174,19 +176,19 @@ DMAMEM static uint16_t spectrum[C12880_NUM_CHANNELS+88+1];         // buffer,
 // ST Low perdiod minimum     375/f_clk 
 // ST Low is start configuration
 // ST High starts sequence
-// Integration starts at 4th clock cycle
-// Integration finishes 51 cycles after ST switched to low
-// Integration time is ST high length plus 48 clks
+// Integration/Exposure starts at 4th clock cycle
+// Integration finishes 51 cycles after ST switches to low
+// Integration/Exposure time is ST high length plus 48 clks
 // Signal appears at 89th pulse and continues 288 pulses
 // ----------------------------------------------------------
-// 1 Run clock one cycle  
+// 1 Run clock for one cycle  
 // 2 Set ST_pin high
-// 3 Run clock 3 pulses to start signal integration
-// 4 Run clock until up to 48 clock cycles before integration time expires
+// 3 Run clock 3 pulses to start sensor integration/exposure
+// 4 Run clock until 48 clock cycles before integration time expires
 // 5 Set ST pin low
-// 6 Run clock for 48 cycles until end of integration time
+// 6 Run clock for 48 cycles until end of integration/exposure time
 // 7 Run clock for an other 40 cycles 
-// 8 Run clock for C12880_NUM_CHANNELS and run ADC
+// 8 Run clock for C12880_NUM_CHANNELS and enable ADC
 // ----------------------------------------------------------
 void counterloop() { 
   count--; // allways count down when clk rises
@@ -232,7 +234,7 @@ void counterloop() {
 // Programmable Delay Block Interrupt Service Routine
 // Not needed
 void pdb_isr(void) {
-  PDB0_SC = (PDB_SC_TRGSEL(00) | PDB_SC_PDBEN | PDB_SC_PDBIE ) | PDB_SC_LDOK; // (also clears interrupt flag)
+  PDB0_SC = (PDB_SC_TRGSEL(00) | PDB_SC_PDBEN | PDB_SC_PDBIE ) | PDB_SC_LDOK; // clears interrupt flag
   pdbticks++;
 }
 
@@ -241,7 +243,7 @@ void pdb_isr(void) {
 // Its not needed when DMA is enabled
 void adc0_isr() {
   adcticks++;
-  adcval = adc->adc0->readSingle(); // read and clear interrupt,  dma still occurs
+  adcval = adc->adc0->readSingle(); // read and clear interrupt,  dma occurs
 }
 
 /******************************************************************************/
@@ -261,7 +263,7 @@ void EOS_isr() {
   spectrumReady = true;
   // Stop ADC
   dma0.disable();
-  adc->disableDMA(ADC_0);
+  adc->adc0->disableDMA(ADC_0);
   adc->adc0->stopExtTrigPDB(true);
   if (flashEnabled) { digitalWriteFast(FLASH_TRIGGER,LOW); }
   if (LASEREnabled) { digitalWriteFast(LASER_TRIGGER,LOW); }
@@ -277,8 +279,8 @@ void STfall_isr() {
   last_read = 0;
   adc->adc0->startExtTrigPDB(false);               // enable external trigger and its interrupt
   adc->adc0->startSingleRead(SPEC_VIDEO);          // call this to setup everything before the pdb starts
-  adc->enableInterrupts(ADC_0);                    // not needed, but usuful here
-  adc->enableDMA(ADC_0);                           // set ADC_SC2_DMAEN
+  adc->adc0->enableInterrupts(adc0_isr);           // not needed, but usuful here
+  adc->adc0->enableDMA();                          // set ADC_SC2_DMAEN
   dma0.enable();                                   //
 }
 
@@ -291,17 +293,17 @@ void setADC() {
   // VeryHigh sampling speed requires low impednace output buffer at signal stage to provide sufficient current
   // Enabeling programmable gain amplifier might reduce ADC speed
   // ADC reference is 3.3V max 
-  adc->disablePGA(ADC_0);        
-  adc->setReference(ADC_REFERENCE::REF_3V3, ADC_0);
-  adc->setAveraging(0, ADC_0); 
-  adc->setResolution(12, ADC_0); 
-  adc->disableCompare(ADC_0);
+  adc->adc0->disablePGA();        
+  adc->adc0->setReference(ADC_REFERENCE::REF_3V3);
+  adc->adc0->setAveraging(0); 
+  adc->adc0->setResolution(12); 
+  adc->adc0->disableCompare();
   if (highSpeed) {
-    adc->setConversionSpeed(ADC_CONVERSION_SPEED::VERY_HIGH_SPEED, ADC_0);
-    adc->setSamplingSpeed(ADC_SAMPLING_SPEED::VERY_HIGH_SPEED, ADC_0);
+    adc->adc0->setConversionSpeed(ADC_CONVERSION_SPEED::VERY_HIGH_SPEED);
+    adc->adc0->setSamplingSpeed(ADC_SAMPLING_SPEED::VERY_HIGH_SPEED);
   } else {
-    adc->setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED, ADC_0);
-    adc->setSamplingSpeed(ADC_SAMPLING_SPEED::HIGH_SPEED, ADC_0);
+    adc->adc0->setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED);
+    adc->adc0->setSamplingSpeed(ADC_SAMPLING_SPEED::HIGH_SPEED);
   }
   // Initialize dma
   dma0.source((volatile uint16_t&)ADC0_RA);
@@ -312,7 +314,7 @@ void setADC() {
   dma0.attachInterrupt(&dma0_isr);
 }
 
-// SET CLK signal on CLK pin using PWM modulation. Usually duty cycle is 50.0%
+// SET CLK signal on CLK pin using PWM modulation. Duty cycle is 50.0%
 void setCLK(uint16_t CLK_pin, uint32_t CLK_Frequency, float PWM_Duty, float PWM_MaxValue, uint16_t PWM_Resolution){
   analogWriteResolution(PWM_Resolution);
   analogWriteFrequency(CLK_pin, CLK_Frequency);
@@ -324,10 +326,10 @@ void setCLK(uint16_t CLK_pin, uint32_t CLK_Frequency, float PWM_Duty, float PWM_
 // Either transmit ASCII data separated by "," or transmit binary data 
 // At end of transmission send "new line" character.
 //
-// Currently tranmit all data to host.
+// Currently transmit all data to host.
 // If things look allright it should transmit 
 //    [last_read-C12880_NUM_CHANNELS+1 .. last_read] 
-//    additional data could be used to determine ADC offset and noise
+//    extra readings can be used to determine ADC offset and noise
 void transmitSpectrum(uint16_t *data) {
   size_t i;
   if (aPlotter) {
@@ -369,7 +371,7 @@ void printHelp() {
   mySerial.println("Transmit spectrum buffer:.......... r");
   mySerial.println("Use/dont use binary transmission: . B/b");
   mySerial.println("Format for Arduino serial plotter:  A/a");
-  mySerial.println("================== 2019 ===================");
+  mySerial.println("============== 2019 - 2020 ================");
   mySerial.println("Urs Utzinger, Tucson, Arizona, USA         ");
   mySerial.println("===========================================");
 }
@@ -493,7 +495,7 @@ void processInstruction(String instruction) {
 
 float GetMaxPWMFreqValue(uint32_t FREQ, uint16_t PWM_Resolution)
 {
-  /* for Teensy CPU frequency 24MHZ 48MHZ 72MHZ 96MHZ 120MHZ  */
+  /* for Teensy 3.x CPU frequency 24MHZ 48MHZ 72MHZ 96MHZ 120MHZ  */
   int FREQ_pointer=-1;
   float PWM_ideal_frequency[5][15]
   {
@@ -547,7 +549,7 @@ void setup(){
 
   attachInterrupt(INT_Pin,  counterloop,  RISING);
   attachInterrupt(SPEC_EOS, EOS_isr,      RISING);  // end of scan to stop ADC and illumination
-  attachInterrupt(SPEC_ST,  STfall_isr,   FALLING); // shortly before video starts to start ADC
+  attachInterrupt(SPEC_ST,  STfall_isr,   FALLING); // shortly before video starts ADC
 
   // Start Clock
   setCLK(CLK_Pin, CLK_Frequency, PWM_Duty, PWM_MaxValue, PWM_Resolution);
@@ -566,7 +568,7 @@ int    bytesread;
 void loop(){
   if (mySerial.available()) {
      if (myState == waiting) {
-       bytesread=mySerial.readBytesUntil('\n', inBuff, 16); // Read from serial until CR is read or timeout exceeded
+       bytesread = mySerial.readBytesUntil('\n', inBuff, 16); // Read from serial until CR is read or timeout exceeded
        inBuff[bytesread]='\0';
        String instruction = String(inBuff);
        processInstruction(instruction);
@@ -582,4 +584,4 @@ void loop(){
       spectrumReady = false;
     }
   } 
-}
+} 
